c3ca78e6625d3183fcbf196bde7ef3533be4eade
beznosikova <tanya.beznosikova@avivi.com.ua>
2017-12-14 22:33:30 +0200

    Revert "fixed bugs"
    This reverts commit 02dffe776af462c8f02273709c17dcbfd7de76d6.

--- lib/animal.php
+++ lib/animal.php
@@ -1,55 +1,67 @@
 <?php
-class Animal {
-
+class Animal
+{
     public $limbsNumber;
     public $age;
 
-    public function __construct($age, $limbsNumber){
+    public function __construct($age, $limbsNumber)
+    {
         $this->age = $age;
         $this->limbsNumber = $limbsNumber;
     }
 
-    protected function live(){
+    protected function live()
+    {
         echo "<hr>Animal::live()<br>";
     }
 
-    public function run(){
+    public function run()
+    {
         echo "<hr>Animal::run()<br>";
         $this->live();
         echo "Number of limbs:".$this->limbsNumber."<br>";
     }
 
-    public function comeToMe() {
+    public function comeToMe()
+    {
         echo "<hr>Animal::comeToMe()<br>";
         $this->live();
         if ($this->age > 10 || $this->limbsNumber <= 1){
             return false;
-        } else {
+        }
+        else{
             $this->run();
             return true;
         }
     }
 }
 
-class Snake extends Animal {
-    public function __construct($age) {
+class Snake extends Animal
+{
+    public function __construct($age)
+    {
         parent::__construct($age, $limbsNumber = 0);
     }
 
-    public function run() {
+    public function run()
+    {
         echo "<hr>Snake::run()<br>";
-        $this->live();
+        parent::live();
         $this->snake();
     }
 
-    public function snake() {
+    public function snake()
+    {
         echo "<hr>Snake::snake()<br>";
     }
 }
 
-class Robot extends Animal {
-    protected function live() {
+class Robot extends Animal
+{
+    protected function live()
+    {
         echo "<hr>Robot::live()<br>";
         echo "killAllHumans<br>";
     }
+
 }
