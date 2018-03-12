a884c7e0a211fb55679441a84fa8cedb36911feb
beznosikova <tanya.beznosikova@avivi.com.ua>
2018-03-12 23:12:05 +0200

    adapter

---
+++ patterns/structural/Adapter.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Шаблон «Адаптер» позволяет помещать несовместимый объект в обёртку, чтобы он оказался совместимым с другим классом.
+ */
+
+interface Lion
+{
+    public function roar();
+}
+
+class AfricanLion implements Lion
+{
+    public function roar()
+    {
+    }
+}
+
+class AsianLion implements Lion
+{
+    public function roar()
+    {
+    }
+}
+
+
+class Hunter
+{
+    public function hunt(Lion $lion)
+    {
+    }
+}
+
+
+// Это нужно добавить
+class WildDog
+{
+    public function bark()
+    {
+    }
+}
+
+// Адаптер вокруг собаки сделает её совместимой с охотником
+class WildDogAdapter implements Lion
+{
+    protected $dog;
+
+    public function __construct(WildDog $dog)
+    {
+        $this->dog = $dog;
+    }
+
+    public function roar()
+    {
+        $this->dog->bark();
+    }
+}
+
+
+$wildDog = new WildDog();
+$wildDogAdapter = new WildDogAdapter($wildDog);
+
+$hunter = new Hunter();
+$hunter->hunt($wildDogAdapter);
